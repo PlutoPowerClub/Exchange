99e8ec50a40e262fe88eed88ac8506842ea1650c
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2024-07-02 07:38:08 +0100

    Starter text

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ rt = app.route
 
 @rt("/")
 def get():
-  return Title("FastHTML"), H1("Hello World!")
+  return Title("Starfish Exchange"), H1("Starfish Exchange"), P("Welcome to the Starfish Exchange!"), A("Click here to learn more", href="/about")
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", default=8000)))
